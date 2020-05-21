d7806396a6766c3c2358d86b10414ac764db34e2
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-05-21 16:24:34 -0500

    feat(inputs): add radio component

---
+++ src/components/Inputs/RFRadio.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const Radio = ({ options, input }) => {
+  return options.map(option => (
+    <div key={option.id}>
+      <input
+        id={option.id}
+        {...input}
+        type="radio"
+        value={option.value}
+        checked={option.value === input.value}
+      />
+      <label htmlFor={option.id}> {option.label}</label>
+    </div>
+  ))
+}
+
+Radio.propTypes = {
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired
+    })
+  )
+}
+
+export { Radio }
