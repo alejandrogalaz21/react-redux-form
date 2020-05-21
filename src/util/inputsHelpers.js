32490380383b34e9f05c75879cdc9eab9864be9e
Jonathan Rodriguez Sanchez <jonathan.rodriguezs@softtek.com>
2020-05-21 16:23:47 -0500

    chore: validation messages

--- src/util/inputsHelpers.js
+++ src/util/inputsHelpers.js
@@ -1,41 +1,43 @@
 export const required = value =>
-  value || typeof value === 'number' ? undefined : 'Required'
+  value || typeof value === 'number' ? undefined : 'Requerido'
 
 export const maxLength = max => value =>
-  value && value.length > max ? `Must be ${max} characters or less` : undefined
+  value && value.length > max ? `Máximo ${max} caracteres` : undefined
 
 export const maxLength15 = maxLength(15)
 
 export const minLength = min => value =>
-  value && value.length < min ? `Must be ${min} characters or more` : undefined
+  value && value.length < min ? `Mínimo ${min} caracteres` : undefined
 
 export const minLength2 = minLength(2)
 
 export const number = value =>
-  value && isNaN(Number(value)) ? 'Must be a number' : undefined
+  value && isNaN(Number(value)) ? 'Debe ser número' : undefined
 
 export const minValue = min => value =>
-  value && value < min ? `Must be at least ${min}` : undefined
+  value && value < min ? `Valor mínimo ${min}` : undefined
 
 export const minValue13 = minValue(13)
 
 export const email = value =>
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
-    ? 'Invalid email address'
+    ? 'Correo electrónico no válido'
     : undefined
 
 export const tooYoung = value =>
-  value && value < 13 ? 'You do not meet the minimum age requirement!' : undefined
+  value && value < 13 ? 'No cumple la edad mínima' : undefined
 
 export const aol = value =>
   value && /.+@aol\.com/.test(value)
     ? 'Really? You still use AOL for your email?'
     : undefined
 
-export const alphaNumeric = value =>
-  value && /[^a-zA-Z0-9 ]/i.test(value) ? 'Only alphanumeric characters' : undefined
+export const alphanumeric = value =>
+  value && /[^a-zA-Z0-9 ]/i.test(value)
+    ? 'Sólo se aceptan caracteres alfanúmericos'
+    : undefined
 
 export const phoneNumber = value =>
   value && !/^(0|[1-9][0-9]{9})$/i.test(value)
-    ? 'Invalid phone number, must be 10 digits'
+    ? 'Número de télefono inválido, deben ser 10 dígitos'
     : undefined
